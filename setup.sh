f8c30167a55bc9661f54c073d9fcfdbe8da4e44a
ashish <ay3524@gmail.com>
2026-02-20 22:27:17 -0800

    feat: Remove --skip-next-steps from MCP installer
    The `--skip-next-steps` argument is removed from the MCP installation command in `setup.sh`. This allows the installer to run its subsequent steps by default.

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ install_mcp() {
   require_cmd curl
 
   echo "Downloading and running MCP installer from: ${MCP_INSTALL_URL}"
-  curl -fsSL "${MCP_INSTALL_URL}" | bash -s -- --skip-next-steps "$@"
+  curl -fsSL "${MCP_INSTALL_URL}" | bash -s -- "$@"
 }
 
 install_skills() {
